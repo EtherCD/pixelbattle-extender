0da9b77f0bda386c82cdd299925968e951e2e713
EtherCD <ethernetcoder@gmail.com>
2025-07-29 15:37:41 +0300

    Now the project starts after assembly

---
+++ src/main/java/com/pixelbattle/extender/Main.java
@@ -0,0 +1,9 @@
+package com.pixelbattle.extender;
+
+import com.pixelbattle.extender.launchers.MainLauncher;
+
+public class Main {
+    public static void main(String[] args) {
+        MainLauncher.main(args);
+    }
+}
